b18b4e556ea0e31dfea29e52b64c92631f75a8bf
ciphernexus0 <ciphernexus@outlook.com>
2025-05-03 22:09:55 -0400

    Create text-adventure.py
    initial framework

---
+++ text-adventure.py
@@ -0,0 +1,11 @@
+answer_yes = ["Yes", "Y", "yes", "y"]
+answer_no = ["No", "N", "no", "n"]
+
+print("""
+WELCOME! LET'S START THE ADVENTURE
+
+You are standing outside of your house and you see a man running towards you and asking for urgent shelter.
+
+Will you provide shelter to him. (Yes / No)
+""")
+
